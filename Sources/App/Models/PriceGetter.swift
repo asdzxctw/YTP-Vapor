c7798d9c6a558e97fdc759990935f4e2e75b15ef
asdzxctw <asdzxcasdzxctw@yahoo.com.tw>
2016-11-22 23:46:40 +0800

    新增歷史查詢

--- Sources/App/Models/PriceGetter.swift
+++ Sources/App/Models/PriceGetter.swift
@@ -5,11 +5,47 @@ import Foundation
 class StockPriceGetter{
     var stockNum:Int
     var stockUrl:URL
+    var hisStockURL:URL
     
     init(number:Int) {
         self.stockNum = number
         stockUrl = URL(string: "https://tw.stock.yahoo.com/q/q?s=\(number)")!
+        hisStockURL = URL(string: "http://www.cnyes.com/twstock/ps_historyprice/\(number).htm")!
     }
+    
+    
+    func getHistoryPrice(startDate:String,endDate:String) -> [String:String] {
+        var reDic:[String:String] = [:]
+        let sema = DispatchSemaphore( value: 0 )
+        print(startDate)
+        print(endDate)
+        
+        let session = URLSession(configuration: .default)
+        var request: URLRequest = URLRequest(url: hisStockURL)
+        request.httpMethod = "POST"
+        request.httpBody = "ctl00$ContentPlaceHolder1$startText=\(startDate)&ctl00$ContentPlaceHolder1$endText=\(endDate)".data(using: String.Encoding.utf8)
+        
+        
+        let dataTask: URLSessionDataTask = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            
+            let jiDoc = Ji(htmlData: data!)!
+            let endPriceNodes = jiDoc.xPath("//div[@id='main3']/div[@class='mbx bd3']/div[@class='tab']/table/tr/td[5]")
+            let timeNodes = jiDoc.xPath("//div[@id='main3']/div[@class='mbx bd3']/div[@class='tab']/table/tr/td[1]")
+            
+            for index in 0...endPriceNodes!.count-1{
+                reDic["\(timeNodes![index].content!)"] = "\(endPriceNodes![index].content!)"
+            }
+            //reDic = ["Yo":"Hoho"]
+            sema.signal()
+            
+        }
+        dataTask.resume()
+        sema.wait()
+        
+        return reDic
+        
+    }
+    
     
     func getPriceNow() -> [String:String] {
         var reDic:[String:String]!
